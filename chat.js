e322baf41f1b9688c4275cdb48d378099ec3f406
ali7887a <82027484+ali7887a@users.noreply.github.com>
2021-07-21 13:18:55 +0430

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,43 @@
+class Chatroom{
+    constructor(room , username){
+        this.room = room;
+        this.username = username;
+        this.chats = db.collection('pipiha');
+        this.unsub;
+    }
+    async addChat(message){
+        //format of chat
+        const now = new Date();
+        const chat = {
+        message: message,
+        username: this.username,
+        room: this.room,
+        created_at: firebase.firestore.Timestamp.fromDate(now)
+        };
+        const response = await this.chats.add(chat)
+        return response;
+    }
+    getChats(callback){
+        this.unsub=this.chats
+           .where('room' , '==' , this.room)
+           .orderBy('created_at')
+            .onSnapshot(snapshot => {snapshot.docChanges().forEach(change => {
+                if(change.type === 'added'){
+                    callback(change.doc.data());
+                }
+                
+            });
+            });
+    }
+    updateName(username){
+        this.username = username;
+        localStorage.setItem('username' , username);
+    }
+    updateRoom(room){
+        this.room = room
+        console.log('room has been updated')
+        if(this.unsub){
+            this.unsub();
+        }
+    }
+}
